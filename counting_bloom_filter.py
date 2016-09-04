0864241536c56e0cb2024c7622e7a1d7fc1d8aaf
Jinfeng Liu <owenliuchina@163.com>
2016-09-05 00:23:33 +0800

    new_cbf
    this is a counting bloom filter in python3

--- counting_bloom_filter.py
+++ counting_bloom_filter.py
@@ -1,69 +1,60 @@
 import hashlib
 import math
 import random
+class Counting_bloom_filter(object):
+    def __init__(self, el_num, p):
+        self.el_num = el_num
+        self.positive = p
+        self.gen_para()
+        self.init_bf()
 
-def gen_para(ft_len, b_num, p):
-#   print('ft_len', ft_len)
-    if ft_len:
-        length = ft_len
-        h_num = math.ceil(-(math.log(p)/math.log(2)))
-    else:
-        length = math.ceil(-(b_num* math.log(p))/((math.log(2)) ** 2))
-        h_num = math.ceil((length/b_num)*math.log(2))
-#       print(length, h_num)
-    return length, h_num
+    def gen_para(self):
+        self.length = math.ceil(-(self.el_num* math.log(self.positive))/((math.log(2)) ** 2))
+        self.hash_num = math.ceil((self.length/self.el_num)*math.log(2))
 
-def init_bf(length):   
-    return [0] * length
+    def init_bf(self):   
+        self.c_bf = [0] * self.length
 
-def add_to_bf(val, vec, para):
-    l = []
-#   print(vec)
-    for i in range(para[1]):
-        loc = short_hash(val, para[0], i+1)
-        l.append(loc)
-#       print('loc ',loc)
-        vec[loc] += 1
-#   print(vec[loc],'plus 1, now is', )
-    return vec
+    def add_to_cbf(self, val):
 
-def short_hash(val, length, seed):
-#   print('length is ', length)
-    sha1 = hashlib.sha1()
-    sha1.update(val.encode('utf-8'))
-#   print(sha1.digest())
-    val =  int(sha1.hexdigest(), 16) * seed
-#   print('val',val%length)
-    return val % length
+        for i in range(self.hash_num):
+            loc = self.short_hash(val, i+1)
 
-def look_up(val, vec, para):
-    print('enter look_up')
-    for i in range(para[1]):
-        loc = short_hash(val, para[0], i+1)   
-        if vec[loc]:
-            continue
+            self.c_bf[loc] += 1
+            
+    def delete(self, val):
+        if self.look_up(val):
+            for i in range(self.hash_num):
+                loc = self.short_hash(val, i+1)
+                self.c_bf[loc] -= 1
+            print("delete success")
         else:
-            return False
-    return True
+            print("ERROR:this element doesn't exist in bloom filter")
+            
+    def short_hash(self, val, seed):
+        sha1 = hashlib.sha1()
+        sha1.update(str(val).encode('utf-8'))
+        val =  int(sha1.hexdigest(), 16) * seed
+        return val % self.length
 
+    def look_up(self, val):
+        for i in range(self.hash_num):
+            loc = self.short_hash(val, i+1)   
+            if self.c_bf[loc]:
+                continue
+            else:
+                print("Don't find", str(val))
+                return False
+        print("find", str(val))
+        return True
 def main():
-    b_num, p = 50, 0.1
-    para = gen_para(b_num, p)
-    print(para)
-    bloom_filter = init_bf(para[0])
-    print(bloom_filter)
-    while True:
-        function = input("(查找（F）添加（A）退出（Q）查看（D）): ")
-        if not function: break
-        if function.lower() == 'q': break
-        if function.lower() == 'a':
-            buffer = input('input a string: ')
-            bloom_filter = add_to_bf(buffer, bloom_filter, para)
-        if function.lower() == 'f':
-            buffer = input('input a string: ')
-            print(look_up(buffer, bloom_filter, para))
-        if function.lower() == 'd':
-            print(bloom_filter)
-    
+    a = Counting_bloom_filter(3,0.001)
+    print(a.c_bf)
+    a.add_to_cbf(10)
+    print(a.c_bf)
+    print(a.look_up(2))
+    a.delete(10)
+    print(a.c_bf)
+    a.delete(2)
+    print(a.c_bf)    
 if __name__ == '__main__': main()
-
